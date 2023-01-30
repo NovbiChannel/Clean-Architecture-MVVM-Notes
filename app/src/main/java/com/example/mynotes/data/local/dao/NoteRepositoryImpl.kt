cc14c1256c2fe4f0c3e2baf0a4e3a6fc05d30845
NovbiChannel <chaglovnikita@gmail.com>
2023-01-30 17:53:35 +0500

    I created a DAO interface

--- app/src/main/java/com/example/mynotes/data/local/dao/NoteRepositoryImpl.kt
+++ app/src/main/java/com/example/mynotes/data/local/dao/NoteRepositoryImpl.kt
@@ -1,4 +1,23 @@
 package com.example.mynotes.data.local.dao
 
-class NoteRepositoryImpl {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.mynotes.domain.model.Note
+
+@Dao
+interface NoteRepositoryImpl {
+
+    @Insert
+    suspend fun insertNote(note: Note)
+
+    @Query("SELECT * FROM note")
+    suspend fun getAllNotes(): List<Note>
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM note WHERE id=:noteId")
+    suspend fun getNoteById(noteId: Long): Note
 }
